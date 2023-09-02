710d1fd08b6d529f4f1ae9868e700371e8147df7
JayDubbewar <jaydubbewar@gmail.com>
2023-09-03 00:12:39 +0530

    success in getting response

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,7 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
-  getData(){
-    let data = this.http.get('https://localhost:7239/api/Stud');
-    return data;
+  getData() {
+    return this.http.get('https://localhost:7239/api/Stud');
   }
 }
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+import { ApiService } from '../api.service';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
+  data: any;
 
+  constructor(private apiService: ApiService) {
+    // this.data = this.apiService.getData();
+    // console.log(JSON.stringify(this.data));
+    this.fetchData();
+  }
+  responseData: any
+  fetchData() {
+    this.apiService.getData().subscribe(
+      (data) => {
+        console.log('Data received:', data); 
+          this.responseData = data;
+      }
+    );
+  }
+  
+  
 }
