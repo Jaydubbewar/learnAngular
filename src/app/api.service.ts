92dedb5f7384fd36039de84568b82a5f81a797f9
JayDubbewar <jaydubbewar@gmail.com>
2023-09-03 10:33:01 +0530

    after delete

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -13,4 +13,10 @@ export class ApiService {
   getData() {
     return this.http.get('https://localhost:7239/api/Stud');
   }
+  deleteData(id:number){
+    return this.http.delete(`https://localhost:7239/api/Stud/${id}`)
+  }
+  fetchRec(id:number){
+    return this.http.get(`https://localhost:7239/api/Stud/${id}`)
+  }
 }
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -12,17 +12,27 @@ export class LandingComponent {
   constructor(private apiService: ApiService) {
     // this.data = this.apiService.getData();
     // console.log(JSON.stringify(this.data));
-    this.fetchData();
+    // this.fetchData();
   }
   responseData: any
   fetchData() {
     this.apiService.getData().subscribe(
       (data) => {
-        console.log('Data received:', data); 
-          this.responseData = data;
+        console.log('Data received:', data);
+        this.responseData = data;
       }
     );
   }
-  
-  
+  delData(id: number) {
+    this.apiService.deleteData(id).subscribe(()=>{this.fetchData()});
+  }
+  oneRec(id: any) {
+    this.apiService.fetchRec(parseInt(id.value, 10)).subscribe((data) => {
+      console.log('Data received:', data);
+      this.responseData = data;
+    }
+    );
+  }
+
+
 }
